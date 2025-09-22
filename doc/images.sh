c58d886be9b55094450a8b8f9457213acb1a7bd9
Chaim Halbert <chaim.leib.halbert@gmail.com>
2025-09-22 04:49:13 -0700

    doc: use PNG instead of SVG in README

--- doc/images.sh
+++ doc/images.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 grep '^battery-icon --level' README.md |
   PATH=target/debug /bin/bash
+
+# Convert to PNG, since the SVG with FontAwesome won't render in markdown.
+for f in doc/*.svg; do
+  magick -background none "$f" "${f%.svg}.png"
+  rm "$f"
+done
